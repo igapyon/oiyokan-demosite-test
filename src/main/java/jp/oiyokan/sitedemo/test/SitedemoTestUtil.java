46488096ee54c84219a11e64797772dfd2b33104
Toshiki Iga <igapyon@gmail.com>
2021-04-25 18:35:54 +0900

    save

--- src/main/java/jp/oiyokan/sitedemo/test/SitedemoTestUtil.java
+++ src/main/java/jp/oiyokan/sitedemo/test/SitedemoTestUtil.java
@@ -3,6 +3,11 @@ package jp.oiyokan.sitedemo.test;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.olingo.client.api.ODataClient;
+import org.apache.olingo.client.api.communication.request.retrieve.ODataEntityRequest;
+import org.apache.olingo.client.api.communication.response.ODataRetrieveResponse;
+import org.apache.olingo.client.api.domain.ClientEntity;
+import org.apache.olingo.client.api.domain.ClientPrimitiveValue;
+import org.apache.olingo.client.api.domain.ClientProperty;
 import org.apache.olingo.client.core.ODataClientFactory;
 import org.apache.olingo.commons.api.edm.Edm;
 import org.apache.olingo.commons.api.format.ContentType;
@@ -14,7 +19,7 @@ public class SitedemoTestUtil {
     private static volatile ODataClient client = null;
 
     public static String getServiceUrl() {
-        return SitedemoTestConstants.serviceUrl;
+        return SitedemoTestConstants.SERVICE_URL;
     }
 
     public static final ODataClient getClient() {
@@ -36,4 +41,29 @@ public class SitedemoTestUtil {
 
         return edm;
     }
+
+    /////////////////
+    // Read util
+
+    public static void readEntityOne(String entitySetName, Object keySegment) {
+        ODataEntityRequest<ClientEntity> request = SitedemoTestUtil.getClient().getRetrieveRequestFactory()
+                .getEntityRequest(SitedemoTestUtil.getClient().newURIBuilder(SitedemoTestUtil.getServiceUrl()) //
+                        .appendEntitySetSegment(entitySetName) //
+                        .appendKeySegment(keySegment) //
+                        .build());
+        request.setAccept("application/json;odata.metadata=full");
+
+        final ODataRetrieveResponse<ClientEntity> response = request.execute();
+        final ClientEntity entity = response.getBody();
+        log.info("  EntitySet: " + entitySetName);
+        printEntity(entity);
+    }
+
+    public static void printEntity(ClientEntity entity) {
+        for (ClientProperty property : entity.getProperties()) {
+            ClientPrimitiveValue priValue = property.getPrimitiveValue();
+            log.info("    " + property.getName() + " (" + priValue.getType() + ")" + ": " + property.getValue());
+        }
+    }
+
 }
--- src/test/java/jp/oiyokan/sitedemo/test/ReadEntityOne01Test.java
+++ src/test/java/jp/oiyokan/sitedemo/test/ReadEntityOne01Test.java
@@ -2,32 +2,14 @@ package jp.oiyokan.sitedemo.test;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.olingo.client.api.communication.request.retrieve.ODataEntityRequest;
-import org.apache.olingo.client.api.communication.response.ODataRetrieveResponse;
-import org.apache.olingo.client.api.domain.ClientEntity;
-import org.apache.olingo.client.api.domain.ClientPrimitiveValue;
-import org.apache.olingo.client.api.domain.ClientProperty;
 import org.junit.jupiter.api.Test;
 
 public class ReadEntityOne01Test {
+    @SuppressWarnings("unused")
     private static final Log log = LogFactory.getLog(ReadEntityOne01Test.class);
 
     @Test
     void test01() {
-        ODataEntityRequest<ClientEntity> request = SitedemoTestUtil.getClient().getRetrieveRequestFactory()
-                .getEntityRequest(SitedemoTestUtil.getClient().newURIBuilder(SitedemoTestUtil.getServiceUrl()) //
-                        .appendEntitySetSegment("ODataTests1") //
-                        .appendKeySegment(1) //
-                        .build());
-        request.setAccept("application/json;odata.metadata=full");
-
-        final ODataRetrieveResponse<ClientEntity> response = request.execute();
-        final ClientEntity entity = response.getBody();
-
-        for (ClientProperty property : entity.getProperties()) {
-            ClientPrimitiveValue priValue = property.getPrimitiveValue();
-            log.info("    " + property.getName() + " (" + priValue.getType() + ")" + ": " + property.getValue());
-        }
+        SitedemoTestUtil.readEntityOne("ODataTests1", Integer.valueOf(1));
     }
-
 }
