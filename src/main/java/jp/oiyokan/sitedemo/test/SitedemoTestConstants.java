2305fa8947f6f20bc46540bb001054a91a5fff29
Toshiki Iga <igapyon@gmail.com>
2021-04-25 22:01:35 +0900

    テストを追加。

--- src/main/java/jp/oiyokan/sitedemo/test/SitedemoTestConstants.java
+++ src/main/java/jp/oiyokan/sitedemo/test/SitedemoTestConstants.java
@@ -4,7 +4,7 @@ public class SitedemoTestConstants {
     /**
      * Oiyokan のバージョン番号
      */
-    public static final String VERSION = "1.7.20210425a";
+    public static final String VERSION = "1.7.20210425c";
 
 //  public static final String SERVICE_URL = "https://oiyokan.herokuapp.com/odata4.svc/";
     public static final String SERVICE_URL = "https://oiyokan-devel.herokuapp.com/odata4.svc/";
