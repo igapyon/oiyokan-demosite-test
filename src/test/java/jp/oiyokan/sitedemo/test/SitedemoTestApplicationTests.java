9ea65ea44188a1f3bc21de88aacdafc037678570
Toshiki Iga <igapyon@gmail.com>
2021-04-25 15:51:10 +0900

    initial.

---
+++ src/test/java/jp/oiyokan/sitedemo/test/SitedemoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.oiyokan.sitedemo.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitedemoTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
